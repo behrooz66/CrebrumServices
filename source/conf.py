2d48c8c7cde908cef73015f01814e59ba49923ee
behrooz <behrooz66@gmail.com>
2022-09-08 14:37:07 -0700

    update

--- source/conf.py
+++ source/conf.py
@@ -19,14 +19,6 @@
 
 # -- Project information -----------------------------------------------------
 
-import sphinx_rtd_theme
-
-extensions = [
-    ...
-    'sphinx_rtd_theme',
-]
-
-html_theme = "sphinx_rtd_theme"
 
 
 project = 'awaremd'
@@ -48,12 +40,18 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import sphinx_rtd_theme
+
+
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig',
+    'sphinx_rtd_theme',
 ]
 
+html_theme = "sphinx_rtd_theme"
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['ntemplates']
 
