275185b79cf56e50c75cace3cbf7488481b07495
behrooz <behrooz66@gmail.com>
2022-09-08 14:34:03 -0700

    updated the theme

--- source/conf.py
+++ source/conf.py
@@ -19,6 +19,16 @@
 
 # -- Project information -----------------------------------------------------
 
+import sphinx_rtd_theme
+
+extensions = [
+    ...
+    'sphinx_rtd_theme',
+]
+
+html_theme = "sphinx_rtd_theme"
+
+
 project = 'awaremd'
 copyright = '2022, Behrooz Dalvandi'
 author = 'Behrooz Dalvandi'
